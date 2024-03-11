5cb8dcdf3cc7d6d06387cd55ec342a754d7afdbc
s21383 <s21383@pjwstk.edu.pl>
2024-03-11 22:13:12 +0100

    Zadanie 3 - Generator tekstu Lorem Ipsum

---
+++ Zadania/src/Zadanie3.java
@@ -0,0 +1,34 @@
+public class Zadanie3 {
+    public static void main(String[] args) {
+        System.out.println(generateLoremIpsum(50));
+    }
+
+    public static String generateLoremIpsum(int words){
+        String loremIpsumDemo = """
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                """;
+
+        String[] loremIpsumDemoArray = loremIpsumDemo.split("\\s+|\\.");
+
+        String[] loremIpsum = new String[words];
+        for (int i = 0; i < words; i++) {
+            if( i < 5 ) {
+                loremIpsum[i] = loremIpsumDemoArray[i];
+            } else {
+                int randomInt = getRandomNumber(words - 1);
+                loremIpsum[i] = loremIpsumDemoArray[randomInt];
+            }
+        }
+
+        return new String(String.join(" ", loremIpsum));
+    }
+    public static int getRandomNumber(int max) {
+        int min = 0;
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+}
